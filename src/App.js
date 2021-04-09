7f0d91c711f11e312125649299321627b642f634
Gustavo Messias <gustavo.hmessias96@gmail.com>
2021-04-08 22:06:25 -0300

    feat:create cardVaccine for myVaccine List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import MenuAside from "./components/MenuAside";
 import Routes from "./routes";
+// import {} from "./components/";
+import CardVaccine from "./components/CardVaccine";
 
 function App() {
   return (
     <div>
       <MenuAside />
       <Routes />
+      <CardVaccine />
     </div>
   );
 }

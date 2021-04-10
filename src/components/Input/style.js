d09dc80d2a0ae8359633ecfcc04f444d1d6758ae
Luciano Feder <lucianoo.feder@gmail.com>
2021-04-10 17:31:18 +0000

    feat: implemented checkbox and select types into input

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -28,13 +28,15 @@ export const ErrorMessage = styled.span`
 `;
 
 export const InputStyled = styled.input`
-  width: 100%;
-  height: 30px;
+  width: ${({type}) => type !== 'checkbox' ? '100%' : '20px'};
+  height: ${({type}) => type !== 'checkbox' ? '30px' : '20px'};
   color: ${Theme.colors.text_first};
   background-color: ${Theme.colors.background_second};
   outline: none;
+  margin-left: ${({type}) => type === 'checkbox' && '10px'};
   padding: 0 5px;
-  border: 1px solid
+  border: 1px solid;
+  cursor: ${({type}) => type === 'checkbox' && 'pointer'};
     ${({ error }) => (error ? Theme.colors.error : "transparent")};
   box-shadow: 0 4px 6px
     ${({ error }) =>
@@ -48,3 +50,32 @@ export const InputStyled = styled.input`
     border-color: ${Theme.colors.main_user};
   }
 `;
+
+export const SelectContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+
+`
+
+export const CheckBoxContainer = styled(SelectContainer)``
+
+export const Select = styled.select`
+  height: 30px;
+  min-width: 40px;
+  padding-left: 5px;
+  outline: none;
+  margin-left: 10px;
+  border-radius: 3px;
+  color: ${Theme.colors.text_first};
+  background-color: ${Theme.colors.background_second};
+  padding: 0 5px;
+  border: 1px solid 'transparent';
+  &:hover {
+    border-color: ${darken(0.04, Theme.colors.main_user)};
+  }
+  &:focus {
+    border-color: ${Theme.colors.main_user};
+  }
+`

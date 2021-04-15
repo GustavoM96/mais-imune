3b12d4638018ebf004950d94f98e9d12569e4d00
Gustavo Messias <gustavo.hmessias96@gmail.com>
2021-04-15 11:05:04 -0300

    fix:direct user when the token is invalid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -9,9 +10,10 @@ import "./styles/globals.css";
 
 function App() {
   const dispatchUser = useDispatch((state) => state.user);
+  const history = useHistory();
 
   useEffect(() => {
-    getUser(dispatchUser);
+    getUser(dispatchUser, history);
   }, []);
   return (
     <div className="flex">

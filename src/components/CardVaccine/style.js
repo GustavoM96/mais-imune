46e0d7bcef61605023f841799ec66b769ed62fcc
Gustavo Messias <gustavo.hmessias96@gmail.com>
2021-04-17 19:13:05 -0300

    fix: change justfy content of card and carAsideList with unique height

--- src/components/CardVaccine/style.js
+++ src/components/CardVaccine/style.js
@@ -5,14 +5,13 @@ import { Theme } from "../../styles/colors";
 export const CardStyled = styled.div`
   background-color: ${Theme.colors.background_third};
   width: 200px;
-  height: 180px;
+  min-height: 180px;
   border-radius: 5px;
   box-shadow: 0px 5px 10px -5px;
   margin: 0.5rem;
   padding: 25px;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
 `;
 
 export const TitleStyled = styled.h6`

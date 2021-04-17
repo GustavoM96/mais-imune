8c842115a432ce5c86a4738ec02e959ca667e436
Wesley <wrodrigo33@gmail.com>
2021-04-17 16:43:00 -0300

    refact: cards size in dashboard

--- src/components/CardDashboard/styles.js
+++ src/components/CardDashboard/styles.js
@@ -7,15 +7,7 @@ export const Container = styled.div`
   background: ${Theme.colors.background_second};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   margin: 0.8rem;
-<<<<<<< HEAD
-  @media (max-height: 800px) {
     height: 14rem;
-  }
-=======
-  /* @media (max-height: 800px) {
-    height: 10rem;
-  } */
->>>>>>> e81acffbee48067760d9e573aafbeb91a0d7e850
 
   h4,
   p {

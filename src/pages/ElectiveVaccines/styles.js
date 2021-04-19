43efe33964f46620625563b907f00d6ffffa24c6
Wesley <wrodrigo33@gmail.com>
2021-04-19 08:23:03 -0300

    bug-fix: fixed elective vaccine page

--- src/pages/ElectiveVaccines/styles.js
+++ src/pages/ElectiveVaccines/styles.js
@@ -1,18 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 90%;
+  padding: 1rem;
+  @media (min-width: 800px) {
+    max-height: 96vh;
+  }
   h2 {
     margin-top: 0;
   }
+
   h2,
   h3 {
     font-weight: 700;
     margin-left: 1.8rem;
   }
-  @media (max-width: 800px) {
-    width: 100%;
-    max-width: 1000px;
+
+  @media (min-width: 800px) {
+    width: 65%;
+    max-width: 1063px;
   }
-  width: 65%;
-  padding: 1rem;
 `;
